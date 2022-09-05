d81ea25eb1f3ee63ce3db91c18f73cf6bd632c6b
BeshoyMorad <besh0morta@gmail.com>
2022-09-05 19:13:44 +0200

    middleware to check if the file already exist

---
+++ src/middlewares/fileExists.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import path from "path";
+import fs from "fs";
+
+export default (req: Request, res: Response, next: NextFunction) => {
+  // check if an image with that width and height exists or no
+  const fileNames = fs.readdirSync(path.join(__dirname, `../../images/after/`));
+
+  const image = `${req.query.fileName}_${req.query.width}_${req.query.height}.jpg`;
+
+  if (fileNames.includes(image)) {
+    console.log("found the image");
+
+    return res.sendFile(path.join(__dirname, `../../images/after/${image}`));
+  } else {
+    //go to resize to process the image
+    next();
+  }
+};
