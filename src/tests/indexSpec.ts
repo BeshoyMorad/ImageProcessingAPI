a9c4008de1641bfd8522a4c8b524556b66007ae4
BeshoyMorad <besh0morta@gmail.com>
2022-09-06 15:46:48 +0200

    installed supertest

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,7 +1,11 @@
-import Add from "../index";
+import supertest from "supertest";
+import app from "../index";
 
-describe("This is a simple test", () => {
-  it("Add 5 + 6 will be 11", () => {
-    expect(Add(5, 6)).toEqual(11);
+const request = supertest(app);
+describe("Test endpoint responses", () => {
+  it("gets the api endpoint", async (done) => {
+    const response = await request.get("/api");
+    expect(response.status).toBe(200);
+    done();
   });
 });
