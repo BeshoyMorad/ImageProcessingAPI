269d1682ff53fa88c558b40d5467c4e1b925c2d4
BeshoyMorad <besh0morta@gmail.com>
2022-09-08 18:33:11 +0200

    testing the resizing the right way

--- src/tests/resizeSpec.ts
+++ src/tests/resizeSpec.ts
@@ -1,5 +1,7 @@
 import supertest from "supertest";
 import app from "../index";
+import path from "path";
+import fs from "fs";
 
 const request = supertest(app);
 describe("Testing resizing images", () => {
@@ -10,10 +12,19 @@ describe("Testing resizing images", () => {
     expect(response.status).toBe(404);
   });
 
-  it("Sending correct parameters", async () => {
-    const response = await request
+  it("Sending correct parameters and check if the image has been processed", async () => {
+    //check if the file it should generate exists
+    const imagesPath: string = path.join(__dirname, `../../images/after`);
+    const fileNames: string[] = fs.readdirSync(imagesPath);
+
+    if (fileNames.includes(`fjord_500_500.jpg`)) {
+      fs.unlinkSync(imagesPath + "/fjord_500_500.jpg");
+    }
+
+    await request
       .get("/api/resize")
       .query({ fileName: "fjord", width: "500", height: "500" });
-    expect(response.status).toBe(200);
+
+    expect(fileNames).toContain("fjord_500_500.jpg");
   });
 });
